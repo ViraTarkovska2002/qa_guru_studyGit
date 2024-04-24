60ff1baebde1df4f76e2a65ad87bfcf3fd5ff2cb
Vira <wiara2002@gmail.com>
2024-04-24 14:52:35 +0300

    new test added

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+public class NewTest {
+    @Test
+    public void newTest(){
+        Assertions.assertTrue(3>2);
+    }
+}
