034908edcdb65f9d42e22520a0eff78935b78fb3
Vira <wiara2002@gmail.com>
2024-04-24 21:18:16 +0300

    change 3 -> 10

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 public class NewTest {
     @Test
     public void newTest(){
-        int i = 3;
+        int i = 10;
         System.out.println("This is the best test");
         Assertions.assertTrue(i>2);
     }
